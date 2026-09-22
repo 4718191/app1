e0ea7fdd1c332d51c54a65b17dc1f5248e6bd268
191 <hth4513@naver.com>
2026-09-22 18:32:47 +0900

    사진 기능 추가.
    PhotosPicker 추가 (수정 화면이니까 food.imageData를 직접 읽고 씀)

--- EditFoodView.swift
+++ EditFoodView.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct EditFoodView: View {
     @Bindable var food: Food
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
+    @State private var selectedItem: PhotosPickerItem?
 
     var body: some View {
         NavigationStack {
@@ -16,6 +18,19 @@ struct EditFoodView: View {
                         Text(cat.rawValue).tag(cat)
                     }
                 }
+
+                Section {
+                    PhotosPicker(selection: $selectedItem, matching: .images) {
+                        if let imageData = food.imageData, let uiImage = UIImage(data: imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 150)
+                        } else {
+                            Label("사진 추가", systemImage: "camera")
+                        }
+                    }
+                }
             }
             .navigationTitle("항목 수정")
             .toolbar {
@@ -23,6 +38,13 @@ struct EditFoodView: View {
                     Button("완료") {
                         try? modelContext.save()
                         dismiss()
+                    }
+                }
+            }
+            .onChange(of: selectedItem) { _, newItem in
+                Task {
+                    if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                        food.imageData = data
                     }
                 }
             }
