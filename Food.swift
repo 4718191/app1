56b8e7999b8ad9cb4c92fffc90c785cb46b557bc
191 <hth4513@naver.com>
2026-09-22 18:20:01 +0900

    수정
    카테고리별 분류 추가.
    enum FoodCategory 새로 추가 (냉장/냉동/실온 세 가지 값)
    Food 클래스에 var category: FoodCategory 속성 추가

--- Food.swift
+++ Food.swift
@@ -1,15 +1,23 @@
 import Foundation
 import SwiftData
 
+enum FoodCategory: String, CaseIterable, Codable {
+    case fridge = "냉장"
+    case freezer = "냉동"
+    case room = "실온"
+}
+
 @Model
 class Food {
     var id: UUID = UUID()
     var name: String
     var expiryDate: Date
+    var category: FoodCategory
 
-    init(name: String, expiryDate: Date) {
+    init(name: String, expiryDate: Date, category: FoodCategory = .fridge) {
         self.name = name
         self.expiryDate = expiryDate
+        self.category = category
     }
 
     var daysRemaining: Int {
